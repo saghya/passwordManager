b41b84f94a9cd67227dd3d13e6f35a7ebcd3524d
saghya <saghyattila77@gmail.com>
2023-10-31 11:47:27 +0100

    optimize

--- Firmware/Core/Src/encoder.c
+++ Firmware/Core/Src/encoder.c
@@ -1,18 +1,14 @@
 #include <stdlib.h>
 #include "encoder.h"
 
-int16_t enc      = 0;
-int16_t prev_enc = 0;
+static int16_t prev_enc = 0;
 
 int8_t encoderDelta()
 {
-    int8_t retval;
-    enc = ENC_CNT;
-    if (abs(enc - prev_enc) > 2) {
-        retval   = enc - prev_enc < 0 ? 1 : -1;
-        prev_enc = enc;
-    } else {
-        retval = 0;
+    int8_t retval = 0;
+    if (abs((int16_t)ENC_CNT - prev_enc) > 3) {
+        retval   = ((int16_t)ENC_CNT - prev_enc < 0) ? 1 : -1;
+        prev_enc = (int16_t)ENC_CNT;
     }
     return retval;
 }
