3bbe5470636ebb2832007a17d7b9ee66857cce7c
saghya <saghyattila77@gmail.com>
2023-11-04 13:12:38 +0100

    bugfix

--- Firmware/Core/Inc/flash.h
+++ Firmware/Core/Inc/flash.h
@@ -7,12 +7,9 @@
 #define NUM_OF_PAGES    (64)
 #define FIRST_PAGE_ADDR (0x8000000)
 
-void     Flash_Read_Data(uint32_t StartPageAddress, uint64_t *RxBuf,
-                         uint16_t numberofdoublewords);
-uint32_t Flash_Write_Data(uint32_t StartPageAddress, uint64_t *Data,
-                          uint16_t numberofdoublewords);
-uint32_t Flash_Write_Page(uint32_t Address, uint64_t *Data,
-                          uint16_t numberofdoublewords);
+void     Flash_Read_Data(uint32_t StartPageAddress, uint64_t *RxBuf, uint16_t numberofdoublewords);
+uint32_t Flash_Write_Data(uint32_t StartPageAddress, uint64_t *Data, uint16_t numberofdoublewords);
+uint32_t Flash_Write_Page(uint32_t Address, uint64_t *Data, uint16_t numberofdoublewords);
 
 #endif // __FLASH_H
 
--- Firmware/Core/Src/flash.c
+++ Firmware/Core/Src/flash.c
@@ -4,36 +4,27 @@
 #include <string.h>
 #include <stdlib.h>
 
-static inline uint32_t getPage(uint32_t address)
-{
-    return (address - FIRST_PAGE_ADDR) / FLASH_PAGE_SIZE;
-}
+static inline uint32_t getPage(uint32_t address) { return (address - FIRST_PAGE_ADDR) / FLASH_PAGE_SIZE; }
 
-uint32_t Flash_Write_Page(uint32_t Address, uint64_t *Data,
-                          uint16_t numberofdoublewords)
+uint32_t Flash_Write_Page(uint32_t Address, uint64_t *Data, uint16_t numberofdoublewords)
 {
     uint16_t numofbytes        = numberofdoublewords * sizeof(uint64_t);
     uint32_t page_shift_before = Address % FLASH_PAGE_SIZE;
     uint32_t StartPageAddress  = Address - page_shift_before;
     uint32_t EndAddress        = Address + numofbytes;
-    uint32_t page_shift_after =
-        FLASH_PAGE_SIZE - (EndAddress % FLASH_PAGE_SIZE);
+    uint32_t page_shift_after  = FLASH_PAGE_SIZE - (EndAddress % FLASH_PAGE_SIZE);
     if (EndAddress - StartPageAddress > 2 * FLASH_PAGE_SIZE) {
         return -1;
     }
     uint8_t buff[2 * FLASH_PAGE_SIZE] = {0};
     memcpy(buff, (uint8_t *)StartPageAddress, page_shift_before);
     memcpy(buff + page_shift_before, (uint8_t *)Data, numofbytes);
-    memcpy(buff + page_shift_before + numofbytes, (uint8_t *)EndAddress,
-           page_shift_after);
+    memcpy(buff + page_shift_before + numofbytes, (uint8_t *)EndAddress, page_shift_after);
 
-    return Flash_Write_Data(
-        StartPageAddress, (uint64_t *)buff,
-        (page_shift_before + numofbytes + page_shift_after) / 8);
+    return Flash_Write_Data(StartPageAddress, (uint64_t *)buff, (page_shift_before + numofbytes + page_shift_after) / 8);
 }
 
-uint32_t Flash_Write_Data(uint32_t StartPageAddress, uint64_t *Data,
-                          uint16_t numberofdoublewords)
+uint32_t Flash_Write_Data(uint32_t StartPageAddress, uint64_t *Data, uint16_t numberofdoublewords)
 {
     static FLASH_EraseInitTypeDef EraseInitStruct;
     uint32_t                      PAGEError;
@@ -45,7 +36,7 @@ uint32_t Flash_Write_Data(uint32_t StartPageAddress, uint64_t *Data,
     /* Erase the user Flash area*/
 
     uint32_t StartPage     = getPage(StartPageAddress);
-    uint32_t EndPageAdress = StartPageAddress + numberofdoublewords * 8;
+    uint32_t EndPageAdress = StartPageAddress + numberofdoublewords * 8 - 8;
     uint32_t EndPage       = getPage(EndPageAdress);
 
     /* Fill EraseInit structure*/
@@ -61,8 +52,7 @@ uint32_t Flash_Write_Data(uint32_t StartPageAddress, uint64_t *Data,
     /* Program the user Flash area doubleword by doubleword*/
 
     for (uint32_t i = 0; i < numberofdoublewords; i++) {
-        if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, StartPageAddress,
-                              Data[i]) == HAL_OK) {
+        if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, StartPageAddress, Data[i]) == HAL_OK) {
             StartPageAddress += 8;
         } else {
             /* Error occurred while writing data in Flash memory*/
@@ -77,8 +67,7 @@ uint32_t Flash_Write_Data(uint32_t StartPageAddress, uint64_t *Data,
     return 0;
 }
 
-void Flash_Read_Data(uint32_t StartPageAddress, uint64_t *RxBuf,
-                     uint16_t numberofdoublewords)
+void Flash_Read_Data(uint32_t StartPageAddress, uint64_t *RxBuf, uint16_t numberofdoublewords)
 {
     while (1) {
         *RxBuf = *(__IO uint64_t *)StartPageAddress;
